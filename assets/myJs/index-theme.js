fb20f0a9540a3dab8d590c6064bdc0085008259b
lingdianshiren <2077358365@qq.com>
2023-07-17 23:17:55 +0800

    ma

--- assets/myJs/index-theme.js
+++ assets/myJs/index-theme.js
@@ -1,14 +1,13 @@
-var totalTheme = "light";
-var background_theme_light = "linear-gradient(to top, #d5d4d0 0%, #d5d4d0 1%, #eeeeec 31%, #efeeec 75%, #e9e9e7 100%)fixed";
-var background_theme_dark = "linear-gradient(to top, #29323c 0%, #485563 100%)fixed";
+var totalTheme = {
+    id: 0,
+    themes: ["light", "dark"]
+};
+eleThemes = ["--background-theme-color", "--general-theme-color"]
 function changeThemes() {
-    if (totalTheme === "dark") {
-        document.documentElement.style.setProperty("--background-theme-color", background_theme_light);
-        totalTheme = "light";
-    } else {
-        document.documentElement.style.setProperty("--background-theme-color", background_theme_dark);
-        totalTheme = "dark";
+    totalTheme.id = 1 - totalTheme.id;
+    for (let i = 0; i < eleThemes.length; i++) {
+        var str = "var(" + eleThemes[i] + "-" + totalTheme.themes[totalTheme.id] + ")";
+        document.documentElement.style.setProperty(eleThemes[i], str);
     }
-    console.log(totalTheme);
-    console.log(document.documentElement.style.getPropertyValue("--background-theme-color"));
+    window.getComputedStyle(document.body).getPropertyValue('transform');
 }
